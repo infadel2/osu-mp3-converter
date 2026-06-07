ceb00a00c30894540198a06a70f74db327e2bb4a
lille <servalservalserval@proton.me>
2026-06-07 19:19:49 -0400

    force server metadata + print whenever conv is finished

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import eyed3
 import ffmpeg
 import base64
 import argparse
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
@@ -20,11 +21,11 @@ if temp_folder.is_dir():
 temp_folder.mkdir(parents=True, exist_ok=False)
 
 @eel.expose
-def taskGen(youtube_link, md_serv, md_yt, spek):
-    add_post_metadata = md_serv
-    add_youtube_metadata = md_yt
+def taskGen(youtube_link, metadata, spek):
+    add_youtube_metadata = metadata
     generate_spectrogram = spek
-    print('I: Starting task', youtube_link, 'with options', md_serv, md_yt, spek)
+    print('I: Starting task', youtube_link, 'with options', metadata, spek)
+    start = time.perf_counter()
 
     # yt-dlp section (self explanatory, we also save brain power and lines with the ffmpeg postprocessor)
     defaults = {
@@ -49,16 +50,14 @@ def taskGen(youtube_link, md_serv, md_yt, spek):
     os.rename(str(temp_folder) + '/final.mp3', str(temp_folder) + '/' + video_id + '.mp3')
 
     # eyed3 section (we use eyed3 here because ffmpeg sucks at handling comment metadata for some reason)
-    if add_post_metadata:
-        audiofile = eyed3.load(str(temp_folder) + '/' +  video_id + '.mp3')
-        if add_youtube_metadata:
-            # this is disabled by default because it almost always
-            # sucks due to how ppl upload music to youtube
-            audiofile.tag.artist = video_artist
-            audiofile.tag.title = video_title
-            audiofile.tag.album = video_title
-        audiofile.tag.comments.set('check out https://github.com/infadel2/osu-mp3-converter')
-        audiofile.tag.save(version=(2, 3, 0)) # apparently 2.4 breaks windows id3 compatibility
+    audiofile = eyed3.load(str(temp_folder) + '/' +  video_id + '.mp3')
+    if add_youtube_metadata:
+        # this is disabled by default because it almost always sucks due to how ppl upload music to youtube
+        audiofile.tag.artist = video_artist
+        audiofile.tag.title = video_title
+        audiofile.tag.album = video_title
+    audiofile.tag.comments.set('check out https://github.com/infadel2/osu-mp3-converter')
+    audiofile.tag.save(version=(2, 3, 0)) # apparently 2.4 breaks windows id3 compatibility
 
     # generate a spectrogram (if the user wants)
     if generate_spectrogram:
@@ -86,6 +85,8 @@ def taskGen(youtube_link, md_serv, md_yt, spek):
     with open(str(temp_folder) + '/' + video_id + '.mp3', "rb") as mp3_file:
         base64_string = base64.b64encode(mp3_file.read()).decode("utf-8")
     eel.startDownload(base64_string, video_id)
+    end = time.perf_counter()
+    print(f'I: {youtube_link} was successfully converted in {(end - start):.3f} seconds')
     os.remove(str(temp_folder) + '/' + video_id + '.mp3')
 
 parser = argparse.ArgumentParser()
