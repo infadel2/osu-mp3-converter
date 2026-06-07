6573ff56efdee75732ae473ab3b9e726159e8676
lille <servalservalserval@proton.me>
2026-06-07 14:06:27 -0400

    initial spectrogram code
    ill finish this eventually but not now

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ if temp_folder.is_dir():
     shutil.rmtree(temp_folder)
 temp_folder.mkdir(parents=True, exist_ok=False)
 @eel.expose
-def taskGen(youtube_link, md_serv, md_yt):
+def taskGen(youtube_link, md_serv, md_yt, spek):
     add_post_metadata = md_serv
     add_youtube_metadata = md_yt
-    print('I: Starting task', youtube_link, 'with options', md_serv, md_yt)
+    generate_spectrogram = spek
+    print('I: Starting task', youtube_link, 'with options', md_serv, md_yt, spek)
 
     # yt-dlp section (self explanatory, we also save
     # brain power and lines with the ffmpeg postprocessor)
@@ -51,6 +52,29 @@ def taskGen(youtube_link, md_serv, md_yt):
         audiofile.tag.comments.set('check out https://github.com/infadel2/osu-mp3-converter')
         audiofile.tag.save(version=(2, 3, 0)) # apparently 2.4 breaks windows id3 compatibility
 
+    # generate a spectrogram (if the user wants)
+    if generate_spectrogram:
+        import ffmpeg
+        import numpy as np
+        import matplotlib.pyplot as plt
+        from scipy.io import wavfile
+        (
+            ffmpeg
+            .input(str(temp_folder) + '/' + video_id + '.mp3')
+            .output(str(temp_folder) + '/' + video_id + '-spek.wav')
+            .run()
+        )
+        sample_rate, audio_data = wavfile.read(str(temp_folder) + '/' + video_id + '-spek.wav')
+        if len(audio_data.shape) > 1:
+            audio_data = audio_data[:, 0]
+        plt.figure(figsize=(10, 6))
+        plt.specgram(audio_data, Fs=sample_rate, cmap='gist_heat', NFFT=1024, noverlap=512, vmin=40, vmax=-40)
+        plt.title('generated spectrogram for ' + video_id)
+        plt.ylabel('Hz')
+        plt.xlabel('Time (sec)')
+        plt.colorbar(label='Gain (dB)')
+        plt.savefig(str(temp_folder) + '/' + video_id + '-spek.png', bbox_inches='tight', dpi=300)
+        os.remove(str(temp_folder) + '/' + video_id + '-spek.wav')
     with open(str(temp_folder) + '/' + video_id + '.mp3', "rb") as mp3_file:
         import base64 # this is a bit stupid but like whatever
         base64_string = base64.b64encode(mp3_file.read()).decode("utf-8")
